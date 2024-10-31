743b2c16e3fa1a30d0f7f5efc532bc1d6dc11dfd
AmmarAgeeza <ammarfathy516@gmail.com>
2024-10-31 11:09:41 +0300

    add firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'app/app.dart';
 import 'core/bloc/bloc_observer.dart';
 import 'core/services/service_locator.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   await Future.wait([
     serviceLocatorInit(),
   ]);
